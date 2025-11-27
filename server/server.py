6747be05a9485ed11ea633db4dde0ea76f2463df
wachawich <guy.wpth@gmail.com>
2025-11-27 16:08:44 +0700

    [Update] Docker

--- server/server.py
+++ server/server.py
@@ -16,7 +16,7 @@ from groq import Groq
 from search import rag_search
 from QA import ask
 
-load_dotenv("/opt/airflow/.env")
+load_dotenv("/opt/.env")
 # load_dotenv()
 
 pc = Pinecone(api_key=os.getenv("PINE_CONE_API_KEY"))
